87d3176e9b16b9733cbe873231b03b2735f604c5
CoderOK <97104439+2CoderOK@users.noreply.github.com>
2022-01-04 20:45:57 +0200

    fix stats display

--- sunflower.py
+++ sunflower.py
@@ -13,21 +13,21 @@ while height > 0:
 
     w = random.choice(list(weather.keys()))
     print(f"The weather is {w}")
-
+    print(f'Sunflower`s height is {height}"\n\n')
+    
     if good_days > 0:
         height += 1
         good_days -= 1
     else:
         height += weather[w]
-    print(f'Sunflower`s height is {height}"\n\n')
-
+    
     if height > max_height:
         max_height = height
 
     day += 1
 
 print("Your sunflower is gone ...\n")
-print(f"Sunflower survived {day} days")
+print(f"Sunflower survived {day-1} days")
 print(f'Sunflower`s max height was {max_height}"\n\n')
 
 print("Good luck with your next sunflower")
